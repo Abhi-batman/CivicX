1859e7a592d0131ebca5f72b0d5e4638dcb04417
Ddristi Biswas <ddristibiswas617@gmail.com>
2025-11-30 02:32:26 +0530

    Add cloudinary utility for image uploads

--- backend/src/controllers/report.controller.js
+++ backend/src/controllers/report.controller.js
@@ -125,3 +125,9 @@ const deleteReport = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, "Report deleted successfully"));
 });
+
+export {submitReport,
+        updateDescription,
+        deleteReport
+
+}
