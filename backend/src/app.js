a698c4f26d67d4d7722ff844acc25c954d0715b4
Abhi-batman <babluwayne@gmail.com>
2025-11-29 21:43:02 +0530

    Second

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+// import { User } from "./models/user.model.js";
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+// Send index.html on root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+});
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
+
+//importing the routes
+
+// import userRouter from "./routes/user.route.js";
+// import postRouter from "./routes/post.route.js";
+
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/post", postRouter);
+
+export { app };
---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`DB cant be connected `, error);
+  });
