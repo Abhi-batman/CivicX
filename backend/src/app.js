af2e84be286e3351c9bfcf6d7c5d762c3da4e3fa
Ddristi Biswas <ddristibiswas617@gmail.com>
2025-11-30 04:57:09 +0530

    router

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,8 +32,12 @@ app.use(cookieParser());
 
 import userRouter from "./routers/user.router.js";
 import reportRouter from "./routers/report.router.js";
+import authorityRouter from "./routers/authority.router"
+import csrRouter from "./routers/csr.router.js"
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/report", reportRouter);
+app.use("/api/v1/authority",authorityRouter )
+app.use("/api/v1/csr", csrRouter)
 
 export { app };
