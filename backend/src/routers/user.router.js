b58921f6589a823d1e0c2fc6b537660abd6188fe
Ddristi Biswas <ddristibiswas617@gmail.com>
2025-11-30 02:23:52 +0530

    Add user routes for registration, login, logout, password change, profile update, and photo upload

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,2 +1,34 @@
 import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  updatePassword,
+  updateProfile,
+  updateProfilePhoto,
+  getUser,
+  getReport,
+} from "../controllers/user.controller.js"
+
+
 const router = Router();
+
+router.route("/register").post(
+    upload.single(
+       "profilePhoto"
+    ),
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,  logoutUser)
+
+router.route("/change-password").patch(verifyJWT, updatePassword)
+router.route("/update-profile").patch(verifyJWT, updateProfile)
+router.route("/c/:username").get(verifyJWT,getReport)
+router.route("/current-user").get(verifyJWT,getUser)
+
+router.route("/profile-photo").patch(verifyJWT, upload.single("profilePhoto"), updateProfilePhoto)
+
+export default router
